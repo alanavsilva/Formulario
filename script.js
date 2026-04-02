c813518688167b3cef9299001e2426ec481091b0
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-02 12:42:51 -0300

    Add- Validação idade

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ function validacao() {
     let email = document.getElementById("email").value;
     let telefone = document.getElementById("telefone").value;
     let cpf = document.getElementById("cpf").value;
+    let idade = document.getElementById("idade").value;
 
     if (nome.length < 3) {
-        alert("Nome deve ter no mínimo 3 caracteres");
+        alert("Nome deve ter no mínimo 3 caracteres.");
     }
 
     if (!email.includes("@")) {
-        alert("Email deve ter @");
+        alert("Email deve ter @.");
     }
 
     if (telefone.length < 8) {
-        alert("Telefone deve ter no mínimo 8 dígitos");
+        alert("Telefone deve ter no mínimo 8 dígitos.");
     }
 
     if (cpf.length <= 0) {
-        alert("Cpf obrigatório");
+        alert("Cpf obrigatório.");
+    }
+
+    if (idade <= 18) {
+        alert("Envio bloqueado! Você deve ser maior de idade para adotar.");
     }
 }
